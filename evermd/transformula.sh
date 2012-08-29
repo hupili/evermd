a61934fe3f8eeeb8896d25bc2298624781623ec1
hpl <hpl@moving>
2012-08-29 20:12:25 +0800

    add latex formula translation backend

---
+++ evermd/transformula.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+#Encapsulate the backend of formula translation in this script. 
+
+if [[ $# == 2 ]]; then
+	formula=$1
+	output=$2
+else
+	echo "usage: $0 {formula} {output}"
+	exit 255 
+fi
+
+#url="http://chart.apis.google.com/chart?cht=tx&chl=$formula"
+#
+#Google API is not as powerful as the following codecogs
+#
+#try:
+#   ./transformula.sh '\left\lbrace\begin{matrix}a&a\\b&d\end{matrix}\right.' test.png
+url="http://www.codecogs.com/png.latex?$formula"
+
+wget -O $output "$url"
+
+exit 0 
