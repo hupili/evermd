71fcfe3722a0f0e99e492608c336e0c116adef28
hupili <hpl1989@gmail.com>
2012-11-08 21:11:29 +0800

    larger formula by default

--- evermd/transformula.sh
+++ evermd/transformula.sh
@@ -10,12 +10,20 @@ else
 	exit 255 
 fi
 
+formula="\large $formula"
+
+# Example 1:
 #url="http://chart.apis.google.com/chart?cht=tx&chl=$formula"
 #
 #Google API is not as powerful as the following codecogs
 #
 #try:
 #   ./transformula.sh '\left\lbrace\begin{matrix}a&a\\b&d\end{matrix}\right.' test.png
+
+# Example 2:
+#http://latex.codecogs.com/gif.latex?\huge%20y=m%281-\beta%29
+#The above url will generate a formula with "huge" font
+
 url="http://www.codecogs.com/png.latex?$formula"
 
 if [[ ! -e $output ]]; then
